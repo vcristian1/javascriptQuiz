40edebdf0ed0e8b1b0fa9bec8637cb814265e116
Cristian Vargas <cristian.v0223@gmail.com>
2022-06-10 19:26:10 -0500

    added preventDefault() code to end.js so when you save the page isnt refreshed

--- end.js
+++ end.js
@@ -2,9 +2,7 @@ const username = document.querySelector('#username');
 const saveScoreBtn = document.querySelector('#saveScoreBtn');
 const finalScore = document.querySelector('#finalScore');
 const mostRecentScore = localStorage.getItem('mostRecentScore');
-
 const highScores = JSON.parse(localStorage.getItem("highScores")) || []
-
 const MAX_HIGH_SCORES = 5
 
 finalScore.innerText = mostRecentScore
@@ -13,8 +11,26 @@ username.addEventListener ("keyup", () => {
     saveScoreBtn.disabled = !username.value
 })
 
+saveScoreBtn.addEventListener ("submit", function() {
+    e.preventDefault();
+    const score = {
+        score: mostRecentScore,
+        name: username.value
+    }
+
+    highScores.push(score)
+
+    highScores.sort((a,b) => {
+        return b.score - a.score
+    })
+
+    highScores.splice(5)
+
+    localStorage.setItem("highScores", JSON.stringify(highScores))
+    window.location.assign("/")
+})
+
 saveHighScore = e => {
-    e.preventDefault()
 
     const score = {
         score: mostRecentScore,
@@ -34,3 +50,4 @@ saveHighScore = e => {
 
 
 }
+
